e49c97fabb864eec47c1b981c1d8097669971fd8
chiragyadav9759 <yadavchirag618@gmail.com>
2022-06-02 20:31:03 +0530

    chirag

---
+++ src/logger/logger.js
@@ -0,0 +1,10 @@
+const endpoint = "https://www.functionup.org"
+const batch = "radon"
+
+const welcome = function() {
+    console.log('Welcome to my application. I am Chirag Yadav and a part of FunctionUp Thorium cohort.')
+}
+
+module.exports.welcome = welcome
+
+
